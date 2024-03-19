22937fdf9215545612a5f103b0db65641c0c7af0
NabinTim <timalsinanabin41@gmail.com>
2024-03-19 19:06:37 -0400

    first

---
+++ admin_area/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Dashboard</title>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Rubik+Dirt&display=swap" rel="stylesheet">
+</head>
+<body>
+<header class="header">
+    <a href="#" class="Logo">
+    <img src="image.png" alt="Logo"><a class="title" >Admin Dashboard</a>
+</a>
+        
+<nav class="navbar">
+    <a class="nav-link" href="#">Welcome Admin </a>
+</nav>
+
+        
+    </header>
+    <nav class="navbar2">
+    <a class="nav-link" href="#">Welcome Guest</a>
+    <a class="nav-link" href="#">Login</a>
+</nav>  
+
+    <nav class="navbar2">
+    <a class="nav-link" href="#">Manage Details</a>
+</nav>  
+
+<nav class="navbar3">
+    <div class="navbar-brand">
+        <div class="image1"><img src="min.jpg" alt="Min's Picture" class="brand-image">
+        <span class="brand-name">Min Prasad Timalsina</span></div>
+    </div>
+    <div class="navbar-links">
+        <a class="nav-link" href="#">Insert Product</a>
+        <a class="nav-link" href="#">View Products</a>
+        <a class="nav-link" href="#">Insert Categories</a>
+        <a class="nav-link" href="#">View Brands</a>
+        <a class="nav-link" href="#">All Orders</a>
+        <a class="nav-link" href="#">All Payments</a>
+        <a class="nav-link" href="#">List Users</a>
+        <a class="nav-link" href="#">Logout</a>
+    </div>
+</nav>
+
+
+
+
+</body>
+</html>
