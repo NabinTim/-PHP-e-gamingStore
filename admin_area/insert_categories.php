336fd5f52b3dadc8ec8a09a79ec98e25563289f0
NabinTim <timalsinanabin41@gmail.com>
2024-03-20 00:24:16 -0400

    fixed issues

---
+++ admin_area/insert_categories.php
@@ -0,0 +1,14 @@
+<form action="" method="post" class="first">
+
+<div class="input-group">
+
+<div id="formTab" class="form-tab">
+        <div class="form-content">
+            <!-- Your form content here -->
+            <!-- For now, just a simple message -->
+            <p>This is your form content.</p>
+        </div>
+    </div>
+
+</div>
+</form>
